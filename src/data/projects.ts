9854355a79604cc5eb04ccd313a2a3648760a5e3
iber <iberson-silva@outlook.com>
2025-02-01 21:04:37 -0500

    load landing page

---
+++ src/data/projects.ts
@@ -0,0 +1,52 @@
+const projects = [
+    {
+        description: 'Landing Page Donca ERP',
+        image: '/projects/proyecto1.jpg',
+        link: 'https://landing-offofwork.pages.dev/',
+        technologies: [
+            {
+                icon: '/icon/Svelte.png',
+                name: 'Svelte'
+            },
+            {
+                icon: '/icon/TypeScript.png',
+                name: 'TypeScript'
+            },
+            {
+                icon: '/icon/Tailwind CSS.png',
+                name: 'Tailwind'
+            },
+        ]
+    },
+    {
+        description: 'Portfolio Personal',
+        image: '/projects/proyecto2.jpg',
+        link: 'https://silvaiberson03.netlify.app/',
+        technologies: [
+            {
+                icon: '/icon/Astro.png',
+                name: 'Astro'
+            },
+            {
+                icon: '/icon/JavaScript.png',
+                name: 'JavaScript'
+            },
+            {
+                icon: '/icon/Tailwind CSS.png',
+                name: 'Tailwind'
+            },
+        ]
+    },
+    {
+        description: 'Landing Page Rubio Odontología',
+        image: '/projects/proyecto3.jpg',
+        link: 'https://rubio-odontologia-iytzpl5md-iberson-andsil.vercel.app/',
+        technologies: [
+            {
+                icon: '/icon/Bootstrap.png',
+                name: 'Bootstrap'
+            }
+        ]
+    }
+];
+export default projects;
---
+++ src/data/skills.ts
@@ -0,0 +1,68 @@
+const skills = [
+    {
+        icon: "../../public/icon/Astro.png",
+        name: "Astro"
+    },
+    {
+        icon: "../../public/icon/Angular.png",
+        name: "Angular"
+    },
+    {
+        icon: "../../public/icon/Svelte.png",
+        name: "Svelte"
+    },
+    {
+        icon: "../../public/icon/JavaScript.png",
+        name: "JavaScript"
+    },
+    {
+        icon: "../../public/icon/TypeScript.png",
+        name: "TypeScript"
+    },
+    {
+        icon: "../../public/icon/GitHub.png",
+        name: "GitHub"
+    },
+    {
+        icon: "../../public/icon/Git.png",
+        name: "Git"
+    },
+    {
+        icon: "../../public/icon/CSS3.png",
+        name: "CSS3"
+    },
+    {
+        icon: "../../public/icon/HTML5.png",
+        name: "HTML5"
+    },
+    {
+        icon: "../../public/icon/Linux.png",
+        name: "Linux"
+    },
+    {
+        icon: "../../public/icon/Docker.png",
+        name: "Docker"
+    },
+    {
+        icon: "../../public/icon/Vercel.png",
+        name: "Vercel"
+    },
+    {
+        icon: "../../public/icon/Tailwind CSS.png",
+        name: "Tailwind"
+    },
+    {
+        icon: "../../public/icon/Bootstrap.png",
+        name: "Bootstrap"
+    },
+    {
+        icon: "../../public/icon/Figma.png",
+        name: "Figma"
+    },
+    {
+        icon: "../../public/icon/Photoshop.png",
+        name: "Photoshop"
+    }
+];
+
+export default skills;
