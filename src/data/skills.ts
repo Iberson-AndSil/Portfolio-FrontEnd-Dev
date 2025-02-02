7cfba7ae6385be3d8c6e5c500440feb0deebf627
iber <iberson-silva@outlook.com>
2025-02-01 21:36:01 -0500

    fix solutions root icons

--- src/data/skills.ts
+++ src/data/skills.ts
@@ -1,66 +1,66 @@
 const skills = [
     {
-        icon: "../../public/icon/Astro.png",
+        icon: "/icon/Astro.png",
         name: "Astro"
     },
     {
-        icon: "../../public/icon/Angular.png",
+        icon: "/icon/Angular.png",
         name: "Angular"
     },
     {
-        icon: "../../public/icon/Svelte.png",
+        icon: "/icon/Svelte.png",
         name: "Svelte"
     },
     {
-        icon: "../../public/icon/JavaScript.png",
+        icon: "/icon/JavaScript.png",
         name: "JavaScript"
     },
     {
-        icon: "../../public/icon/TypeScript.png",
+        icon: "/icon/TypeScript.png",
         name: "TypeScript"
     },
     {
-        icon: "../../public/icon/GitHub.png",
+        icon: "/icon/GitHub.png",
         name: "GitHub"
     },
     {
-        icon: "../../public/icon/Git.png",
+        icon: "/icon/Git.png",
         name: "Git"
     },
     {
-        icon: "../../public/icon/CSS3.png",
+        icon: "/icon/CSS3.png",
         name: "CSS3"
     },
     {
-        icon: "../../public/icon/HTML5.png",
+        icon: "/icon/HTML5.png",
         name: "HTML5"
     },
     {
-        icon: "../../public/icon/Linux.png",
+        icon: "/icon/Linux.png",
         name: "Linux"
     },
     {
-        icon: "../../public/icon/Docker.png",
+        icon: "/icon/Docker.png",
         name: "Docker"
     },
     {
-        icon: "../../public/icon/Vercel.png",
+        icon: "/icon/Vercel.png",
         name: "Vercel"
     },
     {
-        icon: "../../public/icon/Tailwind CSS.png",
+        icon: "/icon/Tailwind CSS.png",
         name: "Tailwind"
     },
     {
-        icon: "../../public/icon/Bootstrap.png",
+        icon: "/icon/Bootstrap.png",
         name: "Bootstrap"
     },
     {
-        icon: "../../public/icon/Figma.png",
+        icon: "/icon/Figma.png",
         name: "Figma"
     },
     {
-        icon: "../../public/icon/Photoshop.png",
+        icon: "/icon/Photoshop.png",
         name: "Photoshop"
     }
 ];
