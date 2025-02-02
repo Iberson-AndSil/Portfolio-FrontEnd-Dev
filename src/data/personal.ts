eb4f9695caceb265c3f1e49727de0ca67938c921
iber <iberson-silva@outlook.com>
2025-02-02 11:49:59 -0500

    feat add footer

---
+++ src/data/personal.ts
@@ -0,0 +1,24 @@
+const personal = {
+    title: "Desarrollador Frontend | Est. en Ingeniería de Informática y Sistemas",
+    description: "Desarrollador Frontend con experiencia en la creación de aplicaciones web responsive. Como experiencia realicé mis prácticas preprofesionales en Lead Working Partner, colaboré en la optimización y desarrollo de la versión responsive de su ERP.",
+    contact: [
+        {item: "Tingo María Perú"},
+        {item: "(+51) 900661606"}
+    ],
+    networks: [
+        {
+            icon: '/icon/LinkedIn.png',
+            link: 'https://www.linkedin.com/in/iberson-silva-890532259/'
+        },
+        {
+            icon: '/icon/Outlook.png',
+            link: 'mailto:iberson-silva@outlook.com'
+        },
+        {
+            icon: '/icon/GitHub.png',
+            link: 'https://github.com/Iberson-AndSil'
+        },
+    ]
+}
+
+export default personal;
