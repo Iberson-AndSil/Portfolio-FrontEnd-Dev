f5984c01620850184558efc6fc3870db96766335
Iberson <iberson-silva@outlook.com>
2025-09-05 10:37:05 -0500

    feat: update cv

--- src/data/personal.ts
+++ src/data/personal.ts
@@ -1,7 +1,7 @@
 const personal = {
     title: "Desarrollador Frontend | Est. en Ingeniería de Informática y Sistemas",
     description: "Desarrollador Front-End con experiencia en Angular, Astro, React y Svelte. Especialista en interfaces web modernas y responsivas, aplicando BEM y Tailwind CSS. He desarrollado módulos para ERP, sitios web adaptables y sistemas eCommerce, utilizando Next.js, Ant Design e integración de APIs.",
-    curriculum:'https://drive.google.com/file/d/1Ej4nChAo-uUWNcP6fl3PA9z2zv0myEmS/view?usp=sharing',
+    curriculum:'https://drive.google.com/file/d/1azH60YsUGLzvyqYHR3eWbImjWYPqDgtV/view?usp=drive_link',
     contact: [
         {item: "City: Tingo María, Perú"},
         {item: "Tel: (+51) 900661606"}
